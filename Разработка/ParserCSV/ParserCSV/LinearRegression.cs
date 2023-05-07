4f166b47d73cd1b711049d202d6c69e33834153e
Bolonin  Official <boloninnn@gmail.com>
2023-05-07 18:02:29 +0300

    Создан класс с ллинейной регрессией

---
+++ Разработка/ParserCSV/ParserCSV/LinearRegression.cs
@@ -0,0 +1,159 @@
+﻿namespace ParserCSV
+{
+    internal class LinearRegression
+    {
+        public List<double> X { get; private set; }
+        public double SumX { get; private set; }
+        public double AvrX { get; private set; }
+        public List<double> XAvrX { get; private set; }
+        public List<double> XAvrX2 { get; private set; }
+        public double SumXAvrX2 { get; private set; }
+
+        public List<double> Y { get; private set; }
+        public double SumY { get; private set; }
+        public List<double> SettlementY { get; private set; }
+
+        public List<double> XY { get; private set; }
+        public double SumXY { get; private set; }
+
+        public List<double> X2 { get; private set; }
+        public double SumX2 { get; private set; }
+
+        public List<double> Y2 { get; private set; }
+        public double SumY2 { get; private set; }
+
+        public List<double> E { get; private set; }
+        public List<double> DE { get; private set; }
+        public List<double> DE2 { get; private set; }
+        public double SumDE2 { get; private set; }
+        public List<double> E2 { get; private set; }
+        public double SumE2 { get; private set; }
+
+        public List<double> A { get; private set; }
+        public double SumA { get; private set; }
+        public double AvrA { get; private set; }
+
+        public int Count { get; private set; }
+        public double a { get; private set; }
+        public double ma { get; private set; }
+        public double ta { get; private set; }
+
+        public double b { get; private set; }
+        public double mb { get; private set; }
+        public double tb { get; private set; }
+
+        public double R { get; private set; }
+        public double mR { get; private set; }
+        public double tR { get; private set; }
+
+        public double R2 { get; private set; }
+        public double Ffact { get; private set; }
+        public double Dfact { get; private set; }
+        public LinearRegression(List<double> x, List<double> y)
+        {
+            X = x;
+            Y = y;
+            AuxiliaryValues();
+            LinearRegressionEquations();
+            LinearPairCorrelation();
+            ApproximationError();
+            Fischer();
+            RandomParameterError();
+            Studentstatistics();
+            DurbinWatsonCriteria();
+        }
+        private void AuxiliaryValues()
+        {
+            AvrX = X.Average();
+            XY = new List<double>();
+            X2 = new List<double>();
+            Y2 = new List<double>();
+            XAvrX = new List<double>();
+            XAvrX2 = new List<double>();
+            Count = X.Count;
+            for (int i = 0; i < Count; i++)
+            {
+                XY.Add(X[i] * Y[i]);
+                X2.Add(X[i] * X[i]);
+                Y2.Add(Y[i] * Y[i]);
+                XAvrX.Add(X[i] - AvrX);
+                XAvrX2.Add(XAvrX[i] * XAvrX[i]);
+            }
+            SumXAvrX2 = Sum(XAvrX2);
+            SumX = Sum(X);
+            SumY = Sum(Y);
+            SumXY = Sum(XY);
+            SumX2 = Sum(X2);
+            SumY2 = Sum(Y2);
+        }
+        private void LinearRegressionEquations()
+        {
+            a = (SumX * SumY - Count * SumXY) / (SumX * SumX - Count * SumX2);
+            b = (SumX * SumXY - SumX2 * SumY) / (SumX * SumX - Count * SumX2);
+            SettlementY = new List<double>();
+            E = new List<double>();
+            E2 = new List<double>();
+            A = new List<double>();
+            DE = new List<double>();
+            DE2 = new List<double>();
+            for (int i = 0; i < Count; i++)
+            {
+                SettlementY.Add(a * X[i] + b);
+                E.Add(Y[i] - SettlementY[i]);
+                E2.Add(E[i] * E[i]);
+                A.Add(Math.Abs((E[i]) / (Y[i])));
+                if (i > 0)
+                {
+                    DE.Add(E[i] - E[i - 1]);
+                    DE2.Add(DE[i] * DE[i]);
+                }
+                else
+                {
+                    DE.Add(0);
+                    DE2.Add(0);
+                }
+            }
+            SumE2 = Sum(E2);
+            SumA = Sum(A);
+            SumDE2 = Sum(DE2);
+        }
+        private void LinearPairCorrelation()
+        {
+            R = (Count * SumXY - SumX * SumY) / (Math.Sqrt((Count * SumX2 - SumX * SumX) * (Count * SumY2 - SumY * SumY)));
+            R2 = R * R;
+        }
+        private void ApproximationError()
+        {
+            AvrA = ((SumA / Count)) * 100;
+        }
+        public void Fischer()
+        {
+            Ffact = R2 / (1 - R2) * (Count - 2);
+        }
+        public void RandomParameterError()
+        {
+            ma = Math.Sqrt((1) / (SumXAvrX2) * (SumE2) / (Count - 2));
+            mb = Math.Sqrt((SumE2) / (Count - 2) * (SumX2) / (Count * SumXAvrX2));
+            mR = Math.Sqrt((1 - R2) / (Count - 2));
+        }
+        public void Studentstatistics() 
+        {
+            ta = a / ma;
+            tb = b / mb;
+            tR = R / mR;
+        }
+        public void DurbinWatsonCriteria() 
+        {
+            Dfact = SumDE2 / SumE2;
+        }
+        private double Sum(List<double> values)
+        {
+            double sum = 0;
+            foreach (double value in values)
+            {
+                sum += value;
+            }
+            return sum;
+        }
+    }
+}
--- Разработка/ParserCSV/ParserCSV/Program.cs
+++ Разработка/ParserCSV/ParserCSV/Program.cs
@@ -1,101 +1,49 @@
-﻿using MathNet.Numerics.LinearRegression;
+﻿using Accord.Statistics.Distributions.Univariate;
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Statistics;
 using ParserCSV;
+using System;
+using static System.Formats.Asn1.AsnWriter;
 
 internal class Program
 {
-    public static double maxcorrelation = -1;
-    public static string name = "";
     public static double kx = 0;
-    public static double b = 0;
+    public static double bx = 0;
 
     private static void Main(string[] args)
     {
-
-
-        //var fileputh = "C:\\Users\\Bolofficial\\Desktop\\Cancer_Data.csv";
-        var fileputh = "C:\\Users\\Bolofficial\\Desktop\\1213.csv";
+        var fileputh = "D:\\YandexDisk\\ЯндексДиск\\BOLOFFICIAL\\ЯГТУ\\Бакалавриат\\3 КУРС\\6 СЕМЕСТР\\ИНСТРУМЕНТАЛЬНЫЕ СРЕДСТВА ИНФОРМАЦИОННЫХ СИСТЕМ\\Курсовая работа\\test.csv";
 
         var csv = new CsvData(fileputh);
-        //csv.SetResultColumn("\"diagnosis\"");
+
         csv.SetResultColumn("Y");
-        var names = new List<string>();
-        foreach (var column in csv.Columns)
+
+        var X = new List<double>();
+
+        foreach (var el in csv.GetColumn("X").Value)
         {
-            if (column.Name == csv.ResultColumn.Name || column.Name == "\"id\"") { continue; }
-            CoRR(csv, column.Name);
+            X.Add(double.Parse(el.Replace(".", ",")));
         }
 
-        Console.WriteLine(maxcorrelation + "\t" + name + "\t" +kx + "\t" +b);
+        var Y = new List<double>();
 
-        var bestcolomn = csv.GetColumn(name);
-        for (int i = 0; i < bestcolomn.Value.Count; i++)
+        foreach (var el in csv.ResultColumn.Value)
         {
-            var Y = double.Parse(csv.ResultColumn.Value[i].Replace(".", ","));
-            var Yp = (b * double.Parse(bestcolomn.Value[i].Replace(".", ",")) + kx);
-            var err = ((Math.Abs(Y - Yp)) / Y);
-            Console.WriteLine(Y + "\t" + Yp + "\t" + err);
-        }
-        //Коэффициент корреляции: 0,033387149827745176
-        //Уравнение регрессии: y = 139,046x + 0,4100000000000004
-        //Коэффициент корреляции: 0,6012663889650808
-        //Уравнение регрессии: y = 122,79170287024216x + 0,19946086267809485
-        //Коэффициент корреляции: -0,8492319893023778
-        //Уравнение регрессии: y = 271,42773716755585x + -0,5461463357553197
-        //Коэффициент корреляции: -0,023299412170348775
-        //Уравнение регрессии: y = 140,76240525749353x + -0,013256795127201113
-        //Коэффициент корреляции: 0,027521135185239604
-        //Уравнение регрессии: y = 139,85436893203882x + 0,354368932038835
-        //Коэффициент корреляции: 0,8950121561600756
-        //Уравнение регрессии: y = 109,82090967849432x + 0,38766527083268487
-        //Коэффициент корреляции: -0,8509431530425969
-        //Уравнение регрессии: y = 273,7005018067544x + -0,5560823229818325
-        //Коэффициент корреляции: 0,6428770491572463
-        //Уравнение регрессии: y = -23,081249999999983x + 9,392708333333331
-
-
-        //Коэффициент корреляции: 0,7935660171412694
-        //Уравнение регрессии: y = -0,29696788422769504x + 5,8421902871415154
-    }
-
-    private static void CoRR(CsvData csv, string name)
-    {
-        var x = csv.GetColumn(name).Value;
-        double[] xint = new double[x.Count];
-        for (int i = 0; i < x.Count; i++)
-        {
-            xint[i] = double.Parse(x[i].Replace(".", ","));
-        }
-        var y = csv.ResultColumn.Value;
-        double[] yint = new double[y.Count];
-        for (int i = 0; i < y.Count; i++)
-        {
-            yint[i] = double.Parse(y[i].Replace(".", ","));
-        }
-        Correlation(xint, yint, name);
-        Console.WriteLine();
-    }
-
-    private static void Correlation(double[] x, double[] y, string namecolumn)
-    {
-        try
-        {
-            double correlation = MathNet.Numerics.Statistics.Correlation.Pearson(x, y);
-            var regression = SimpleRegression.Fit(x, y);
-            if (correlation > maxcorrelation)
-            {
-                maxcorrelation = correlation;
-                name = namecolumn;
-                kx = regression.Item1;
-                b = regression.Item2;
-            }
-            Console.WriteLine("Колонка: " + namecolumn);
-            Console.WriteLine("Коэффициент корреляции: " + correlation);
-            Console.WriteLine("Уравнение регрессии: y = " + regression.Item1 + "x + " + regression.Item2);
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("\n\nОшибка в Колонке: " + namecolumn + "\n\n");
+            Y.Add(double.Parse(el.Replace(".", ",")));
         }
 
+        LinearRegression linear = new LinearRegression(X,Y);
+        Console.WriteLine($"Уравнение: Y = {linear.a.ToString("#.##")}x+{linear.b.ToString("#.##")}");
+        Console.WriteLine($"Коэффициент корреляции: {linear.R.ToString("#.##")}");
+        Console.WriteLine($"Коэффициент детерминации: {linear.R2.ToString("#.##")}");
+        Console.WriteLine($"Средняя ошибка аппроксимации: {linear.AvrA.ToString("#.##")}");
+        Console.WriteLine($"F-критерии Фишера: {linear.Ffact.ToString("#.##")}");
+        Console.WriteLine($"Случайная ошибка параметра a: {linear.ma.ToString("#.##")}");
+        Console.WriteLine($"Случайная ошибка параметра b: {linear.mb.ToString("#.##")}");
+        Console.WriteLine($"Случайная ошибка параметра R: {linear.mR.ToString("#.##")}");
+        Console.WriteLine($"t-статистики Стьюдента параметра a: {linear.ta.ToString("#.##")}");
+        Console.WriteLine($"t-статистики Стьюдента параметра b: {linear.tb.ToString("#.##")}");
+        Console.WriteLine($"t-статистики Стьюдента параметра R: {linear.tR.ToString("#.##")}");
+        Console.WriteLine($"Критерии Дарбина-Уотсона: {linear.Dfact.ToString("#.##")}");
     }
 }
