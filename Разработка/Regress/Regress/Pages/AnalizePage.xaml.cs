8d7111154981ff892671373e4b74cb5585c60597
Bolonin  Official <boloninnn@gmail.com>
2023-05-22 20:30:47 +0300

    Посоеднее обновление сохранение файла в PDF

--- Разработка/Regress/Regress/Pages/AnalizePage.xaml.cs
+++ Разработка/Regress/Regress/Pages/AnalizePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using iTextSharp.text;
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using iTextSharp.text;
 using iTextSharp.text.pdf;
 using Microsoft.Win32;
 using OxyPlot;
@@ -7,6 +9,7 @@ using OxyPlot.Series;
 using Regress.Model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -19,7 +22,7 @@ namespace Regress
     /// <summary>
     /// Логика взаимодействия для AnalisePage.xaml
     /// </summary>
-    public partial class AnalisePage : Page
+    public partial class AnalisePage
     {
         private CorrelationRegression _regression;
 
@@ -187,23 +190,34 @@ namespace Regress
 
                     SavePlotViewToPdf(p1, model);
 
-                    string[] data = new string[]
-                   {
-                        "Привет Bolofficial",
-                        "Привет Bolofficial",
-                        "Привет Bolofficial",
-                        "Привет Bolofficial",
-                        "Bolofficial",
-                        "Bolofficial",
-                        "Bolofficial",
-                        "Bolofficial",
-                        "Bolofficial",
-                        "Bolofficial",
-                   };
+
+                    string[] parameters = new string[]
+                    {
+                        "   Уравнение регрессии:",
+                        "   Коэффициент корреляции:",
+                        "   Связь между параметрами:",
+                        "   Коэффициент детерминации:",
+                        "   Средняя ошибка апроксимации:",
+                        "   F-критерий Фишера:",
+                        "   Критерий Дарбина Уотсона:",
+                        "   Автокорреляция:",
+                    };
+
+                    string[] results = new string[]
+                    {
+                        "   "+Label1.Content.ToString(),
+                        "   "+Label2.Content.ToString(),
+                        "   "+Label3.Content.ToString(),
+                        "   "+Label4.Content.ToString(),
+                        "   "+Label5.Content.ToString(),
+                        "   "+Label6.Content.ToString(),
+                        "   "+Label7.Content.ToString(),
+                        "   "+Label8.Content.ToString(),
+                    };
 
                     p2 = Path.Combine(Path.GetDirectoryName(saveFileDialog.FileName), DateTime.Now.ToString("fffffff") + ".pdf");
 
-                    SaveTextToPdf(p2, data);
+                    SaveTableToPdf(p2, parameters, results);
 
                     MergePdfFiles(p1, p2, saveFileDialog.FileName);
 
@@ -224,32 +238,64 @@ namespace Regress
         private void SavePlotViewToPdf(string p1, PlotModel plotModel)
         {
             plotModel.Title = "Correlation - regression analysis in RiverCorr";
+            ((LineSeries)plotModel.Series[0]).MarkerSize = 3;
             var exporter = new PdfExporter();
 
+            var tmp = plotModel.Width;
             exporter.Width = 595;
-            exporter.Height = (595 * 842) / plotModel.Height;
+            exporter.Height = plotModel.Height * 595 / tmp;
 
             using (var stream = File.Create(p1))
             {
                 exporter.Export(plotModel, stream);
             }
+            ((LineSeries)plotModel.Series[0]).MarkerSize = 5;
         }
 
-        private void SaveTextToPdf(string filePath, string[] data)
+        private void SaveTableToPdf(string filePath, string[] column1Data, string[] column2Data)
         {
+            string fileName = System.IO.Path.GetFileName(ProgramData.fileputh);
+            string text = $"\n\n\nКорреляционно - регрессионный анализ для файла {fileName}\n";
             Aspose.Pdf.Document document = new Aspose.Pdf.Document();
             try
             {
+                document.PageInfo.Margin = new Aspose.Pdf.MarginInfo(Aspose.Pdf.PageSize.A4.Width * 0.1, Aspose.Pdf.PageSize.A4.Width * 0.1, 0, 0);
+
                 Aspose.Pdf.Page page = document.Pages.Add();
 
-                foreach (var el in data)
+                var titleText = new Aspose.Pdf.Text.TextFragment(text);
+                titleText.TextState.FontSize = 13;
+                page.Paragraphs.Add(titleText);
+
+                Aspose.Pdf.Table table = new Aspose.Pdf.Table();
+                table.ColumnWidths = "200 300";
+                table.Border = new Aspose.Pdf.BorderInfo(Aspose.Pdf.BorderSide.All, 0.5f);
+                table.DefaultCellBorder = new Aspose.Pdf.BorderInfo(Aspose.Pdf.BorderSide.All, 0.5f);
+                table.DefaultCellTextState.FontSize = 13;
+                table.HorizontalAlignment = Aspose.Pdf.HorizontalAlignment.Center;
+
+                Aspose.Pdf.Row headerRow = table.Rows.Add();
+                headerRow.FixedRowHeight = 20;
+                headerRow.Cells.Add("   Параметр    ");
+                headerRow.Cells.Add("   Значение    ");
+                headerRow.Cells[0].BackgroundColor = Aspose.Pdf.Color.FromRgb(System.Drawing.Color.White);
+                headerRow.Cells[1].BackgroundColor = Aspose.Pdf.Color.FromRgb(System.Drawing.Color.White);
+
+                for (int i = 0; i < column1Data.Length; i++)
                 {
-                    page.Paragraphs.Add(new Aspose.Pdf.Text.TextFragment(el));
+                    Aspose.Pdf.Row dataRow = table.Rows.Add();
+                    dataRow.FixedRowHeight = 30;
+                    dataRow.Cells.Add(column1Data[i]);
+                    dataRow.Cells.Add(column2Data[i]);
+                    dataRow.Cells[0].BackgroundColor = Aspose.Pdf.Color.FromRgb(System.Drawing.Color.White);
+                    dataRow.Cells[1].BackgroundColor = Aspose.Pdf.Color.FromRgb(System.Drawing.Color.White);
                 }
+
+                page.Paragraphs.Add(table);
 
                 document.Save(filePath);
             }
-            catch (iTextSharp.text.DocumentException ex)
+            catch (Exception ex)
             {
                 Console.Error.WriteLine("Ошибка при создании документа PDF: " + ex.Message);
             }
